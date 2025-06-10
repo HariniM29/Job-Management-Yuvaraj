272000d8ef9303c76e82cdf8781579f417a32cc7
Harini <120025325+HariniM29@users.noreply.github.com>
2025-06-10 12:54:49 +0530

    Update main.ts

--- job-admin-backend/src/main.ts
+++ job-admin-backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+global.crypto = require('crypto');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
